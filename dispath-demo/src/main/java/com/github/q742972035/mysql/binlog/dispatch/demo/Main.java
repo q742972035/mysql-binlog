92ea5b6bf42beecdd3a6020e52ba58402c6e9921
zhangyi <742972035@qq.com>
2019-11-18 15:34:26 +0800

    提交部分

--- dispath-demo/src/main/java/com/github/q742972035/mysql/binlog/dispatch/demo/Main.java
+++ dispath-demo/src/main/java/com/github/q742972035/mysql/binlog/dispatch/demo/Main.java
@@ -1,8 +1,26 @@
 package com.github.q742972035.mysql.binlog.dispatch.demo;
 
+import com.github.q742972035.mysql.binlog.dispatch.Runner;
+import com.github.q742972035.mysql.binlog.dispatch.utils.PropertiesUtils;
+import com.github.q742972035.mysql.binlog.expose.build.BinaryLogClientBuild;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Properties properties = PropertiesUtils.getPropertiesByPath("/test.properties", ClassLoader.getSystemClassLoader());
+        BinaryLogClientBuild build = new BinaryLogClientBuild()
+                .setUsername(properties.getProperty("username"))
+                .setPassword(properties.getProperty("password"))
+                .setSchema(properties.getProperty("schema"))
+                .setHostname(properties.getProperty("hostname"));
 
+        Runner runner = new Runner();
+        runner.setClientBuild(build);
+        runner.setScanpackage(properties.getProperty("scanpackage"));
+        runner.init();
+        runner.run();
     }
 }
--- dispath-demo/src/main/java/com/github/q742972035/mysql/binlog/dispatch/demo/handler/UserTableHandler.java
+++ dispath-demo/src/main/java/com/github/q742972035/mysql/binlog/dispatch/demo/handler/UserTableHandler.java
@@ -1,9 +1,30 @@
 package com.github.q742972035.mysql.binlog.dispatch.demo.handler;
 
+import com.github.q742972035.mysql.binlog.dispatch.annotation.Column;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.CurrentPosition;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.NextPosition;
 import com.github.q742972035.mysql.binlog.dispatch.annotation.TableHandler;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.dml.After;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.dml.Before;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.dml.Insert;
+import com.github.q742972035.mysql.binlog.dispatch.annotation.dml.Update;
+import com.github.q742972035.mysql.binlog.dispatch.demo.dto.User;
+
+import java.util.Date;
+import java.util.Map;
 
 @TableHandler(tableName = "user")
 public class UserTableHandler {
 
+    @Insert
+    public void insert(@CurrentPosition long cp, @NextPosition long np
+    ,@CurrentPosition String scp,@NextPosition int inp){
 
+    }
+
+
+    @Update
+    public void update(@Before Map<String,Object> bm, @After Map<String,Object> am){
+
+    }
 }
