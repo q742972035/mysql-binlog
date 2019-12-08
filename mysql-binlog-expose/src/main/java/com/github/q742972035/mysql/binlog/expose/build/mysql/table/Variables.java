2219414b78ed2c7ac58a375ad3cacf2f54a21d67
q742972035 <742972035@qq.com>
2019-12-08 11:06:26 +0800

    添加漏加的类

---
+++ mysql-binlog-expose/src/main/java/com/github/q742972035/mysql/binlog/expose/build/mysql/table/Variables.java
@@ -0,0 +1,22 @@
+package com.github.q742972035.mysql.binlog.expose.build.mysql.table;
+
+public class Variables {
+    private String variableName;
+    private String value;
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
